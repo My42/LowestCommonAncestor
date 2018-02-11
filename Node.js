f53538f424bf20c020ec5762337a18e6fd54b7b6
Mesquita Vincent <vincent.mesquita@epitech.eu>
2018-02-11 17:31:42 +0100

    :tada: Initial commit

---
+++ Node.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(id) {
+	if (id === undefined) throw Error('Invalid id node.');
+	this.id = id;
+	this.left = null;
+	this.right = null;
+    }
+
+    setRight(child) {
+	this.right = child;
+    }
+
+    setLeft(child) {
+	this.left = child;
+    }
+
+}
+
+module.exports = Node;
